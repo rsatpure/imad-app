e0bdf9869abdfa62e09098d2b0ab6cbff875470e
rsatpure <30922781+rsatpure@users.noreply.github.com>
2017-08-30 16:29:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname,  'article-one.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.sendFile(path.join(__dirname,  'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
